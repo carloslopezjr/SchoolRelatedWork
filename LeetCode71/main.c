0e4d5462cc2ee957397d8299f0ff94b04684c486
bravodude <85579815+bravodude@users.noreply.github.com>
2024-02-08 10:58:01 -0600

    algorithm for leetcode simplify

---
+++ LeetCode71/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+char *simplifyPath(char *path)
+{
+
+    char delimeter[] = "/.";
+    char token = strtok(path, delimeter);
+    char *newString = malloc(3000 * sizeof(char));
+    newString[0] = '\0';
+    if (token == NULL)
+    {
+        strcat(newString, "/");
+    }
+
+    while (token != NULL)
+    {
+        strcat(newString, "/");
+        strcat(newString, token);
+        token = strtok(NULL, delimeter);
+    }
+
+    printf("%s\n", newString);
+
+    return newString;
+    
+}
