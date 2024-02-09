f1cd52c6386477b004a77c7abe2e68913d7c08cc
bravodude <85579815+bravodude@users.noreply.github.com>
2024-02-08 21:02:48 -0600

    add unixmax to test if program will overflow

--- ComputerOrg/hw4.c
+++ ComputerOrg/hw4.c
@@ -1,5 +1,6 @@
 // problem for 2.27
 #include <stdio.h>
+#include <limits.h>
 
 int uadd_ok(unsigned x, unsigned y);
 
@@ -7,20 +8,24 @@ int uadd_ok(unsigned x, unsigned y)
 {
 
     unsigned sum = x + y;
-    if (sum >= x) {
+    if (sum >= x)
+    {
 
         return 1; // no overflow
-
-    } else {
+    }
+    else
+    {
 
         return 0; // overflow was caused
     }
-
 }
 
-int main() {
+int main()
+{
+    unsigned x = UINT_MAX - 1; // uni max which is the max limit for the bit size
+    unsigned y = 2; 
 
-    printf("%d\n", uadd_ok(12, 9));
+    printf("%d\n", uadd_ok(x, y));
 
     return 0;
 }
