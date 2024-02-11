f96a3ed447c738ed0b26531a818400160e8fa363
carloslopezjr <85579815+carloslopezjr@users.noreply.github.com>
2024-02-11 12:56:59 -0600

    implement new pop and push functions

--- DataStructuresAssignments/Assignment2/parenthesisStack.c
+++ DataStructuresAssignments/Assignment2/parenthesisStack.c
@@ -2,8 +2,17 @@
 #include <stdlib.h>
 
 // pop function - will go to the last index of the array and remove it
-void pop() {
-    // last index, replace 
+void pop(int *top) {
+    // remove the top element 
+    (*top)--;
+    printf("The top index is now: %d\n", top);
+
+}
+
+void push(int *top, int value, char *stack) {
+    (*top)++;
+    stack[*top] = value;
+    printf("The top index is now: %d\n", top);
 }
 
 // push function - will add a value to the end of the array
@@ -36,22 +45,35 @@ int main() {
     exp[i] = '\0';
 
     int k = 0;
+    int top = -1;
+
+    printf("This is the expression: %s\n", exp);
+
+    char *stack = malloc(size *sizeof(char)); // allocate memory for the stack
 
     while (k <= i) {
+
         if (exp[k] == '(' || exp[k] == '[' || exp[k] == '{') {
-            // push function
-            k++;
-            break;
-        }
-        
-        if (exp[k] == ')' || exp[k] == ']' || exp[k] == '}') {
-            // if top of stack == exp[k]
-                // pop function
             
-            k++;
+            // if there's an open parenthesis, we want to push that to the stack
+            push(&top, exp[k], stack); // there's an issue with the push function
+            k++; // increment k
+        }
+        else if (exp[k] == ')' || exp[k] == ']' || exp[k] == '}') {
+
+            if (stack[top] == exp[k]) {
+                pop(&top); // pop function
+            }
+
+            k++; // increment k
             break;    
+        } else {
+            printf("Couldn't find match\n");
+            k++; // if nothing is met, just continue
         }
     }
+
+    printf("This is the final stack: %s\n", stack);
     
     return 0;
 }
