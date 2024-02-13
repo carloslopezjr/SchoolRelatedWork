d448a2a93db3d314e2f9c58ac38b6173377eaf49
carloslopezjr <85579815+carloslopezjr@users.noreply.github.com>
2024-02-12 22:18:38 -0600

    comment on functions

--- DataStructuresAssignments/Assignment2/infixPostfix.c
+++ DataStructuresAssignments/Assignment2/infixPostfix.c
@@ -3,12 +3,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+// initial stacks
 char inputStack[100];
 char infix[100];
 char postfix[100];
 
+
+// intital value for top
 int top = -1;
 
+
+// looks to see if top is maxed out, if not, increase top and add character
 void push(char ch)
 {
 
@@ -23,6 +29,8 @@ void push(char ch)
     inputStack[top] = ch;
 }
 
+
+// checks value of top and returns 0 or 1 depending if it's at the initial value
 int isEmpty()
 {
 
@@ -36,20 +44,21 @@ int isEmpty()
     }
 }
 
+// checks operation character and returns value that determines precedence
 int order(char operation)
 {
 
     if (operation == '^')
     {
-        return 3;
+        return 3; // high priority
     }
     else if (operation == '/' || operation == '*')
     {
-        return 2;
+        return 2; // mid priority
     }
     else if (operation == '+' || operation == '-')
     {
-        return 1;
+        return 1; // low priority
     }
     else
     {
@@ -57,6 +66,7 @@ int order(char operation)
     }
 }
 
+// creates a temp char and equals it to the last element in the array, and reduces the stack by 1
 char pop()
 {
 
@@ -66,21 +76,22 @@ char pop()
     return ch;
 }
 
+// does the logic to put elements in the postStack
 void postStack()
 {
-    int j = 0;
+    int j = 0; // counter variable for iterations
     char operation;
-    char temp;
+    char temp; // stores value of pop()
     for (int i = 0; infix[i] != '\0'; i++)
     {
         operation = infix[i];
 
-        if (operation == '(')
+        if (operation == '(') // if operation is ( then push into inputStack
         {
             push(operation);
             
         }
-        else if (operation == ')')
+        else if (operation == ')') 
         {
             while ((temp = pop()) != '(')
             {
@@ -88,7 +99,7 @@ void postStack()
             }
             
         }
-        else if (operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == '^')
+        else if (operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == '^') // if operation equals any one of these, it will check to see if the stack is empty and if the top of the stack is greater precedence than the one below it
         {
             while (!isEmpty() && order(inputStack[top]) >= order(operation))
             {
@@ -103,7 +114,7 @@ void postStack()
         }
     }
 
-    while (!isEmpty())
+    while (!isEmpty()) // check to see if stack is empty so it can finish with the '\0'
     {
         postfix[j++] = pop();
     }
